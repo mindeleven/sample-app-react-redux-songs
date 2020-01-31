9a31d29d6faf2492905b3c125f427464d4090b1a
Jürgen Kober <mindeleven@posteo.de>
2020-01-31 13:10:22 +0100

    Some more on the HTML/JSX for the SongList list.

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -5,11 +5,22 @@ class SongList extends Component {
   renderList () {
     return this.props.songs.map((song) => {
       return (
-        <div className='item' key={song.title}>
-
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">
+              Select
+            </button>
+          </div>
+          <div className="content">
+            {song.title}
+          </div>
         </div>
       );
     });
+  }
+
+  render() {
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
